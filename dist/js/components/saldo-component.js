230adeebf6c74979fc25276c167cecf88b941545
Thomaz Lucca <thomazlucca@gmail.com>
2025-03-24 09:15:28 -0300

    projeto final

---
+++ dist/js/components/saldo-component.js
@@ -0,0 +1,15 @@
+import { formatarMoeda } from "../utils/formatters.js";
+import Conta from "../types/Conta.js";
+const elementoSaldo = document.querySelector(".saldo-valor .valor");
+renderizarSaldo();
+function renderizarSaldo() {
+    if (elementoSaldo != null) {
+        elementoSaldo.textContent = formatarMoeda(Conta.getSaldo());
+    }
+}
+const SaldoComponent = {
+    atualizar: function () {
+        renderizarSaldo();
+    }
+};
+export default SaldoComponent;
---
+++ src/components/saldo-component.ts
@@ -0,0 +1,21 @@
+import { formatarMoeda } from "../utils/formatters.js";
+import Conta from "../types/Conta.js";
+
+const elementoSaldo = document.querySelector(".saldo-valor .valor") as HTMLElement;
+
+renderizarSaldo();
+
+function renderizarSaldo(): void {
+    if (elementoSaldo != null) {
+        elementoSaldo.textContent = formatarMoeda(Conta.getSaldo());
+    }
+}
+
+const SaldoComponent = {
+    atualizar: function () {
+        renderizarSaldo()
+    }
+}
+
+export default SaldoComponent;
+
